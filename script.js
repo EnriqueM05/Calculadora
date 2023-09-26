067020be0596dd79516ab475a85e0d68890184a8
EnriqueM05 <64383683+EnriqueM05@users.noreply.github.com>
2023-09-25 19:57:54 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -21,3 +21,23 @@ document.getElementById("resta").onclick = function(){
     
     document.getElementById('countlabel').innerHTML = c;
 }
+
+document.getElementById("divs").onclick = function(){
+    a = document.getElementById('a').value;
+    a = Number(a);
+    b = document.getElementById('b').value;
+    b = Number(b);
+    c = a / b;
+    
+    document.getElementById('countlabel').innerHTML = c;
+}
+
+document.getElementById("multi").onclick = function(){
+    a = document.getElementById('a').value;
+    a = Number(a);
+    b = document.getElementById('b').value;
+    b = Number(b);
+    c = a * b;
+    
+    document.getElementById('countlabel').innerHTML = c;
+}
